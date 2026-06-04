0ab4a01db549f1ac46e3e291fa0f5fa05f7d9e98
yemulambika <ambikayemul2001@gmail.com>
2026-06-04 22:57:00 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/java/1-TwoSum_20260604_225701.java
@@ -0,0 +1,18 @@
+// Last updated: 6/4/2026, 10:57:01 PM
+1class Solution {
+2    public String longestCommonPrefix(String[] strs) {
+3        String ans = "";
+4        String firstWord = strs[0];
+5        for(int i = 0 ; i < firstWord.length() ; i++){
+6            char ch = firstWord.charAt(i);
+7            for(int j = 1 ; j < strs.length ; j++){
+8                if(i >= strs[j].length() || strs[j].charAt(i) != ch){
+9                    return ans;
+10                }
+11                
+12            }
+13            ans += ch;
+14        }
+15        return ans;
+16    }
+17}
