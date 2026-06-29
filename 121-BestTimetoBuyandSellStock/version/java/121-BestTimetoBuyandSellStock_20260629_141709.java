4caac13a94dbe95a7543b4bdf6d8911032d48548
yemulambika <ambikayemul2001@gmail.com>
2026-06-29 14:17:09 +0530

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/java/121-BestTimetoBuyandSellStock_20260629_141709.java
@@ -0,0 +1,17 @@
+// Last updated: 6/29/2026, 2:17:09 PM
+1class Solution {
+2    public int searchInsert(int[] nums, int target) {
+3        int start = 0 ; int end = nums.length -1;
+4        int ans = nums.length;
+5        while(start <= end){
+6            int mid = start + (end - start)/2;
+7            if(nums[mid] >= target){
+8                ans = mid;
+9                end = mid-1;
+10            }else{
+11                start = mid+1;
+12            }
+13        }
+14        return ans;
+15    }
+16}
